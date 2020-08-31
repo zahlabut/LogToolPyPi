1428153d31ad961f5f0c4f862a2c12ffcb8029cf
zahlabut <Bb@123123123>
2020-08-31 21:18:17 +0300

    Add load_conf_file function.

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="LogTool", # Replace with your own username
-    version="0.0.2.7",
-    author="Arkady Shtempler",
-    author_email="arkadysh@gmail.com",
+from setuptools import setup, find_packages
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+setup(
+    name='LogTool',  # Required
+    version='0.0.2.8',  # Required
     description='Extract unique Errors from logs using provided "start time" timestamp',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url="https://github.com/zahlabut/LogToolPyPi",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    author='Arkady Shtempler',
+    author_email='arkadysh@gmail.com',
+    keywords='log files, log, analyze log, statistics, zahlabut, openstack, redhat',
+    package_dir={'': 'src'},  # Optional
+    packages=find_packages(where='src'),  # Required
+    python_requires='>=3.5, <4',
+    package_data={  # Optional
+        'conf_file': ['conf.ini'],
+    },
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/zahlabut/LogToolPyPi/issues',
+        'Articles': 'https://opensource.com/article/20/1/logtool-root-cause-identification',
+        'Source': 'https://github.com/zahlabut/LogToolPyPi/',
+    },
 )
