8db95556f390cf9ebbb6764dba2ee212d23499d6
zahlabut <Bb@123123123>
2020-09-01 11:27:48 +0300

    Making some order

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LogTool", # Replace with your own username
-    version="0.0.2.12",
+    version="0.0.2.13",
     author="Arkady Shtempler",
     author_email="arkadysh@gmail.com",
     description='Extract unique Errors from logs using provided "start time" timestamp',
